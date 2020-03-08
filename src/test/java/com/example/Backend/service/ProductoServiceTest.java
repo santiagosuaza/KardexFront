bd38b76374fc459e25871b69a9e5ebee0d706b97
santiago suaza <sandos12@hotmail.com>
2020-03-08 17:33:31 -0500

    backend con pruebas

--- src/test/java/com/example/Backend/service/ProductoServiceTest.java
+++ src/test/java/com/example/Backend/service/ProductoServiceTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
@@ -37,7 +38,7 @@ class ProductoServiceTest {
         ProductoService productoService = new ProductoService(productoRepository);
         Producto producto = new Producto("Santiago",3,"",0D,"");
         List<Producto> productos = Arrays.asList(producto);
-        when(productoRepository.findAll()).thenReturn(productos);
+       when(productoRepository.findAll()).thenReturn(productos);
         List<Producto> resultado = productoService.consultarTodos();
         assertEquals(productos,resultado);
     }
@@ -49,6 +50,13 @@ class ProductoServiceTest {
 
     @Test
     void bajarStock() {
+       ProductoService productoService = new ProductoService(productoRepository);
+        Producto productoBD = new Producto( "Santiago",3,"",0D,"");
+        Integer cantidadBD =  productoBD.getCantidad();
+        when(productoRepository.findById((any()))).thenReturn(Optional.of(productoBD));
+        when(productoService.bajarStock((any()))).thenReturn(productoBD);
+        assertEquals(productoBD.getCantidad(),
+                (Integer)(cantidadBD-1));
 
 
     }
